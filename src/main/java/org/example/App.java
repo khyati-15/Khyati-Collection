137e471123c1120af569747cb1f58244b335193f
Khyati Khanduja <khyati15khanduja@gmail.com>
2020-03-03 20:37:44 +0530

    EPAM Task Completed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,65 +4,13 @@ package org.example;
  * Hello world!
  *
  */
-import java.util.AbstractList;
-import java.util.Arrays;
+
 import java.util.logging.*;
-
-class MyList<E> extends AbstractList<E>{
-    private int size = 0;
-    private Object data[];
-    private int i = 0;
-
-    public MyList(){
-        data = new Object[10];
-        i=0;
-    }
-
-    @SuppressWarnings("unchecked")
-
-    public E get(int index){
-        return (E)data[index];
-    }
-
-    public boolean add(E e){
-        data[i]=e;
-        i++;
-        if(i>=size)
-            increaseSize();
-        return true;
-    }
-
-    public E remove(int index){
-        if(size< index)
-            return null;
-        Object x=data[index];
-        for(int j=index;j<size-1;j++)
-            data[j]=data[j+1];
-        i--;
-        size--;
-        return (E)x;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    private void increaseSize(){
-        size=data.length*2;
-        data=Arrays.copyOf(data,size);
-    }
-
-    public String toString(){
-        String s="";
-        for (int i = 0; i < this.i; i++)
-            s=s+data[i]+" ";
-        return s;
-    }
-}
+import customList.MyList;
 
 public class App
 {
-    private static final Logger logger = Logger.getLogger((App.class.getName()));
+    private static final Logger logger = Logger.getLogger((org.example.App.class.getName()));
 
     public static void main( String[] args )
     {
